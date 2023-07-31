5cec8256c15755d9588a689a0527cd66c8ff91db
mdatislam <atiq05099@gmail.com>
2023-07-31 17:50:59 +0600

    more

--- RevisionCoreModule/React-Milstone-8/ema-jhon-v2/src/Components/AuthFiles/Shipment.js
+++ RevisionCoreModule/React-Milstone-8/ema-jhon-v2/src/Components/AuthFiles/Shipment.js
@@ -3,7 +3,28 @@ import React from 'react';
 const Shipment = () => {
     return (
         <div>
-            Shipment
+            
+            <form className='form-container'>
+            <h2 style={{textAlign:'center',font:'larger'}}> Plese provid your shipping info:</h2>
+                <div className='input-field'>
+                    <label htmlFor="Name">Name</label>
+                    <input  type='email' placeholder='Enter Your Name' />
+                </div>
+                <div className='input-field'>
+                    <label htmlFor="address">Address</label>
+                    <input  type='email' placeholder='Your Parament address' />
+                </div>
+                <div className='input-field'>
+                    <label htmlFor="email">Email</label>
+                    <input  type='email' placeholder='Your email' />
+                </div>
+                <div className='input-field'>
+                    <label htmlFor="email">Mobile Number</label>
+                    <input  type='email' placeholder='mobile number' />
+                </div>
+                
+            </form>
+
         </div>
     );
 };
